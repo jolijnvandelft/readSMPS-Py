4d6071fcc68cafb0abcfbb5962b6f76530898d6f
Jolijn van Delft <jolijn@jolijns-air.speedport.ip>
2024-11-22 12:16:05 +0100

    Renamed file into create_subproblems.py

---
+++ create_subproblems.py
@@ -0,0 +1,44 @@
+import gurobipy as gb
+import numpy as np
+import os
+import time
+import sys
+
+# Get the directory containing readSMPS
+readsmps_dir = "/Users/jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS"
+
+# Add it to sys.path
+if readsmps_dir not in sys.path:
+    sys.path.append(readsmps_dir)
+
+from readSMPS.decmps_2slp import RandVars
+from readSMPS.decmps_2slp import decompose
+
+
+def main():
+    start = time.time()
+
+    instance = "lands"
+    input_dir = "/Users/Jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS/Input/"
+    output_dir = f"/Users/Jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS/Output/{instance}"
+
+    d = decompose(f"{instance}", input_dir)
+
+    d.find_stage_idx()
+    d.create_master()
+
+    os.makedirs(output_dir, exist_ok=True)
+    master_file = os.path.join(output_dir, "master.lp")
+    d.prob.master_model.write(master_file)
+
+    rand_vars = RandVars(d.name)
+
+    for i, obs in enumerate(rand_vars.observations, start=1):
+        prob = rand_vars.probabilities[i - 1]
+        d.create_sub(obs, prob, i)
+        sub_file = os.path.join(output_dir, f"sub_{i}.lp")
+        d.prob.sub_model.write(sub_file)
+
+
+if __name__ == "__main__":
+    main()
