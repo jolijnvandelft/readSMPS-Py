9e90984293d94d8091514a58857062783c059d51
Jolijn van Delft <jolijn@jolijns-air.speedport.ip>
2024-12-08 20:02:17 +0100

    Fixed small bugs

--- L-shaped.py
+++ L-shaped.py
@@ -1,11 +1,21 @@
-from readSMPS.decmps_2slp import RandVars
 from itertools import product
-from readSMPS.create_master import create_model
 
 import gurobipy as gb
 import numpy as np
 import os
 import time
+import sys
+
+# Get the directory containing readSMPS
+readsmps_dir = "/Users/jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS"
+
+# Add it to sys.path
+if readsmps_dir not in sys.path:
+    sys.path.append(readsmps_dir)
+
+from readSMPS.decmps_2slp import RandVars
+from readSMPS.decmps_2slp import decompose
+
 
 def solve_master(d):
     d.prob.master_model.setParam("OutputFlag", 0)
@@ -19,46 +29,56 @@ def solve_master(d):
         cost_vector = []
 
         for var in d.prob.master_vars:
-            if 'eta' not in var.varName:
+            if "eta" not in var.varName:
                 incmb.append(var.x)
                 cost_vector.append(var.obj)
-            #print("vars",var)
 
         return incmb, cost_vector, objective_value
     else:
-        print("Master problem did not solve to optimality. Status code:", d.prob.master_model.status)
+        print(
+            "Master problem did not solve to optimality. Status code:",
+            d.prob.master_model.status,
+        )
 
         return None
-    
+
+
 def solve_subproblem(d, obs, incmb, iteration, i):
     d.create_LSsub(obs, incmb)
     subproblem_filename = f"iteration_{iteration}_sub_problem_{i+1}.lp"
-    #d.prob.sub_model.write(subproblem_filename)
-    
+    # d.prob.sub_model.write(subproblem_filename)
+
     d.prob.sub_model.setParam("OutputFlag", 0)
     d.prob.sub_model.optimize()
 
     if d.prob.sub_model.status == gb.GRB.OPTIMAL:
-        dual_multipliers = d.prob.sub_model.getAttr('Pi', d.prob.sub_model.getConstrs())
+        dual_multipliers = d.prob.sub_model.getAttr("Pi", d.prob.sub_model.getConstrs())
         objective_value = d.prob.sub_model.objVal
 
         return dual_multipliers, objective_value
     else:
-        print(f"Subproblem {i+1} did not solve to optimality. Status code:", d.prob.sub_model.status)
+        print(
+            f"Subproblem {i+1} did not solve to optimality. Status code:",
+            d.prob.sub_model.status,
+        )
 
         return None, None
-    
+
+
 def add_optimality_cut(d, beta, beta_0, eta, iteration):
     cut_expr = gb.LinExpr()
-    cut_expr.addTerms(beta, d.prob.master_vars[:-1])  # Assuming the last variable is eta
+    cut_expr.addTerms(
+        beta, d.prob.master_vars[:-1]
+    )  # Assuming the last variable is eta
     cut_expr.addTerms(1.0, eta)
 
     cut_name = f"cut_{iteration}"
     d.prob.master_model.addLConstr(cut_expr, gb.GRB.GREATER_EQUAL, beta_0, cut_name)
 
+
 def create_T_mat(d):
-    T_mat_vars = d.prob.mean_model.getVars()[:d.tim.stage_idx_col[1]]
-    T_mat_const = d.prob.mean_model.getConstrs()[d.tim.stage_idx_row[1]:]
+    T_mat_vars = d.prob.mean_model.getVars()[: d.tim.stage_idx_col[1]]
+    T_mat_const = d.prob.mean_model.getConstrs()[d.tim.stage_idx_row[1] :]
 
     T_mat = [[0 for _ in range(len(T_mat_vars))] for _ in range(len(T_mat_const))]
 
@@ -69,16 +89,20 @@ def create_T_mat(d):
 
     return T_mat
 
+
 def create_h_vec(d, obs, incmb):
     d.create_LSsub(obs, incmb)
     h_vec = np.array([const.RHS for const in d.prob.sub_model.getConstrs()])
 
     return h_vec
 
+
 def calculate_probabilities(rand_vars):
     keys_list = [list(rand_vars.dist[i].keys()) for i in range(len(rand_vars.dist))]
     obs_list = list(product(*keys_list))
-    probabilities = [list(rand_vars.dist[i].values()) for i in range(len(rand_vars.dist))]
+    probabilities = [
+        list(rand_vars.dist[i].values()) for i in range(len(rand_vars.dist))
+    ]
 
     probs_scen_list = []
     for scenario in obs_list:
@@ -90,31 +114,38 @@ def calculate_probabilities(rand_vars):
 
     return probs_scen_list
 
+
 def main():
     start = time.time()
 
-    instance = "pgp2"
+    instance = "lands"
     input_dir = "/Users/Jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS/Input/"
     output_dir = f"/Users/Jolijn/Documents/Berlin/Thesis/Code/readSMPS-Py/readSMPS/Output/{instance}"
 
     # Create model
-    d = create_model(instance, input_dir)
+    d = decompose(f"{instance}", input_dir)
+
+    # Create master problem
+    d.find_stage_idx()
+    d.create_master()
 
     # Save the file in the newly created folder
     os.makedirs(output_dir, exist_ok=True)
     output_file = os.path.join(output_dir, "Master_0.lp")
     d.prob.master_model.write(output_file)
-    
+
     # Initialize the RandVars class to read scenario data
     rand_vars = RandVars(d.name)
-    
-    rand_vars_values = [list(rand_vars.dist[i].keys()) for i in range(len(rand_vars.dist))]
+
+    rand_vars_values = [
+        list(rand_vars.dist[i].keys()) for i in range(len(rand_vars.dist))
+    ]
     obs_list = list(product(*rand_vars_values))
     probs_scen_list = calculate_probabilities(rand_vars)
 
-    #Initialize for the while-loop
+    # Initialize for the while-loop
     convergence_criterion = False
-    iteration=1
+    iteration = 1
     incmb, cost_vector, obj_value = solve_master(d)
     lo_bound = -gb.GRB.INFINITY
     up_bound = gb.GRB.INFINITY
@@ -122,10 +153,10 @@ def main():
 
     T_mat = np.array(create_T_mat(d))
 
-    #For initialization of h create the zero solution
-    incmb_zero = [0]*(len(d.prob.master_vars)-1)
+    # For initialization of h create the zero solution
+    incmb_zero = [0] * (len(d.prob.master_vars) - 1)
 
-    while not convergence_criterion: # and iteration < 2:
+    while not convergence_criterion:  # and iteration < 2:
         print("iteration", iteration)
         beta = 0
         beta_0 = 0
@@ -133,7 +164,9 @@ def main():
 
         for i, obs in enumerate(obs_list):
             h_vec = create_h_vec(d, obs, incmb_zero)
-            dual_multipliers, objective_value = solve_subproblem(d, obs, incmb, iteration, i)
+            dual_multipliers, objective_value = solve_subproblem(
+                d, obs, incmb, iteration, i
+            )
 
             # Compute beta_0 and beta
             if dual_multipliers is not None:
@@ -149,16 +182,16 @@ def main():
 
         # Compute upper bound and update up_bound
         upper_bound = np.dot(np.array(cost_vector), np.array(incmb)) + upper_bound_sum
-        
+
         if upper_bound <= up_bound:
             up_bound = upper_bound
 
         eta_var = d.prob.master_vars[-1]
 
-        #Add optimality cut
-        add_optimality_cut(d,beta,beta_0,eta_var,iteration)
+        # Add optimality cut
+        add_optimality_cut(d, beta, beta_0, eta_var, iteration)
 
-        #Solve new master problem
+        # Solve new master problem
         incmb, cost_vector, objective_master_value = solve_master(d)
 
         output_file = os.path.join(output_dir, f"Master_{iteration}.lp")
@@ -180,7 +213,8 @@ def main():
             obj_value = d.prob.master_model.ObjVal
             print(f"Objective Value: {round(obj_value,2)}")
         else:
-            iteration +=1
+            iteration += 1
+
 
 if __name__ == "__main__":
     main()
--- readSMPS/decmps_2slp.py
+++ readSMPS/decmps_2slp.py
@@ -5,84 +5,86 @@ Created on May 4 - 2019
 ---Based on the 2-stage stochastic program structure
 ---Assumption: RHS is random
 ---decompose the 2slp problem into a master and subproblems
----save the distributoin of the random variables and return the 
+---save the distributoin of the random variables and return the
 ---random variables
 
 @author: Siavash Tabrizian - stabrizian@smu.edu
 """
 
-from readCOR  import readcor
+from readCOR import readcor
 from readSTOC import readstoc
-from readTIM  import readtim
+from readTIM import readtim
 
 try:
     import gurobipy as gb
 except ImportError as e:
-    print('Gurobi is needed for building the problem structure!')
+    print("Gurobi is needed for building the problem structure!")
     raise
+
 
 class prob:
     def __init__(self, name):
-        #Import the Mean problem
+        # Import the Mean problem
         cor = readcor(name)
         cor.readfile()
         cor.get_mean()
         self.mean_model = cor.mean_model
-        self.mean_vars   = cor.mean_vars
-        self.mean_const  = cor.mean_const
+        self.mean_vars = cor.mean_vars
+        self.mean_const = cor.mean_const
         self.mean_sol = cor.mean_sol
         self.mean_status = cor.mean_status
         self.mean_objVal = cor.mean_objVal
-        self.mean_var_size   = cor.mean_var_num
+        self.mean_var_size = cor.mean_var_num
         self.mean_const_size = cor.mean_const_num
-        self.master_model = gb.Model('master_')
-        self.master_vars  = self.mean_vars
+        self.master_model = gb.Model("master_")
+        self.master_vars = self.mean_vars
         self.master_const = self.mean_const
-        self.master_var_size  = 0
-        self.master_const_size= 0
-        self.sub_model = gb.Model('sub_')
-        self.sub_vars  = self.mean_vars
+        self.master_var_size = 0
+        self.master_const_size = 0
+        self.sub_model = gb.Model("sub_")
+        self.sub_vars = self.mean_vars
         self.sub_const = self.mean_const
-        self.sub_var_size  = 0
-        self.sub_const_size= 0
-        
+        self.sub_var_size = 0
+        self.sub_const_size = 0
+
 
 class RandVars:
-    def __init__(self,name):
-        #Import stochastic information
+    def __init__(self, name):
+        # Import stochastic information
         stoc = readstoc(name)
         stoc.readfile()
         self.rv = stoc.rv
         self.dist = stoc.dist
         self.cumul_dist = stoc.cumul_dist
         self.rvnum = stoc.rvnum
- 
+
+
 class TIME:
-    def __init__(self,name):
-        #Import time information
+    def __init__(self, name):
+        # Import time information
         tim = readtim(name)
         tim.readfile()
         self.stage_names = tim.stage
-        self.stage_idx_col   = list()
-        self.stage_idx_row   = list()
-        self.stage_size  = tim.stagenum
-        
+        self.stage_idx_col = list()
+        self.stage_idx_row = list()
+        self.stage_size = tim.stagenum
+
+
 class decompose:
-    def __init__(self,name,dirr): # dirr = "./Input/"
+    def __init__(self, name, dirr):  # dirr = "./Input/"
         self.model_name = name
         self.name = dirr + name + "/" + name
-        self.prob = prob(self.name)     #Prob information
-        self.RV   = RandVars(self.name) #Random variabels
-        self.tim  = TIME(self.name)     #Time information(stages)
+        self.prob = prob(self.name)  # Prob information
+        self.RV = RandVars(self.name)  # Random variabels
+        self.tim = TIME(self.name)  # Time information(stages)
 
-        
-    #find the index of stages (rows and columns)
+    # find the index of stages (rows and columns)
     def find_stage_idx(self):
-        #initialize the index structs
+        # initialize the index structs
         self.tim.stage_idx_col = list()
         self.tim.stage_idx_row = list()
-        #find the column indexes (associated with variables)
-        for i in range(0,self.tim.stage_size):
+        # find the column indexes (associated with variables)
+        for i in range(0, self.tim.stage_size):
             count = 0
             for j in self.prob.mean_vars:
                 if self.tim.stage_names[i][0] == j.varname:
@@ -90,50 +92,53 @@ class decompose:
                     break
                 count += 1
 
-            
-        #find the row indexes (associated with constraints)
-        for i in range(0,self.tim.stage_size):
-            emp = [x for x in self.prob.mean_const if 
-                               x.constrname == self.tim.stage_names[i][1]]
+        # find the row indexes (associated with constraints)
+        for i in range(0, self.tim.stage_size):
+            emp = [
+                x
+                for x in self.prob.mean_const
+                if x.constrname == self.tim.stage_names[i][1]
+            ]
             if emp == []:
                 tmp = 0
             else:
                 tmp = self.prob.mean_const.index(emp[0])
-            #add the init and end index of the column of stage i
+            # add the init and end index of the column of stage i
             self.tim.stage_idx_row.append(tmp)
-    
 
     # replace the observations in the proper location of the problem
-    def replaceObs(self,obs,constr):
+    def replaceObs(self, obs, constr):
         obscount = 0
         for c in constr:
-            if c.getAttr('ConstrName') in self.RV.rv:
-                if len(obs) < obscount-1:
+            if c.getAttr("ConstrName") in self.RV.rv:
+                if len(obs) < obscount - 1:
                     print("ERROR: Length of obs is less than rvs")
                     break
                 else:
-                    c.setAttr('RHS',obs[obscount])
+                    c.setAttr("RHS", obs[obscount])
                     self.prob.mean_model.update()
                     obscount += 1
 
         return constr
-    
-    #Create master constraints
+
+    # Create master constraints
     def create_master_constr(self):
-        constr = self.prob.mean_const[:self.tim.stage_idx_row[1]]
+        constr = self.prob.mean_const[: self.tim.stage_idx_row[1]]
         for c in constr:
             empt = gb.LinExpr()
             for v in self.prob.master_vars:
-                empt += self.prob.mean_model.getCoeff(c,v) * v
-            self.prob.master_model.addConstr(empt,c.getAttr('Sense'),c.getAttr('RHS'),c.getAttr('ConstrName'))
+                empt += self.prob.mean_model.getCoeff(c, v) * v
+            self.prob.master_model.addConstr(
+                empt, c.getAttr("Sense"), c.getAttr("RHS"), c.getAttr("ConstrName")
+            )
             self.prob.master_model.update()
         self.prob.master_const = self.prob.master_model.getConstrs()
-    
-    #create LSsub constraints
+
+    # create LSsub constraints
     def create_LSsub_constr(self, obs, incmbt):
         # Get the relevant constraints and replace observations
-        constr = self.prob.mean_const[self.tim.stage_idx_row[1]:]
-        constr = self.replaceObs(obs,constr)
+        constr = self.prob.mean_const[self.tim.stage_idx_row[1] :]
+        constr = self.replaceObs(obs, constr)
 
         # Copy the mean model
         copied_mean_model = self.prob.mean_model.copy()
@@ -143,15 +148,17 @@ class decompose:
         copied_sub_model = self.prob.sub_model.copy()
         copied_sub_model.update()
 
-        copied_sub_model_vars  = copied_sub_model.getVars()
+        copied_sub_model_vars = copied_sub_model.getVars()
 
         for c in constr:
             empt = gb.LinExpr()
             Cx = 0
             for v in copied_sub_model_vars:
                 # Ensure that c and v are correctly referenced in copied_mean_model
-                constr_in_copy = copied_mean_model.getConstrByName(c.getAttr('ConstrName'))
-                var_in_copy = copied_mean_model.getVarByName(v.getAttr('VarName'))
+                constr_in_copy = copied_mean_model.getConstrByName(
+                    c.getAttr("ConstrName")
+                )
+                var_in_copy = copied_mean_model.getVarByName(v.getAttr("VarName"))
 
                 # Access the coefficient in the copied model
                 coeff = copied_mean_model.getCoeff(constr_in_copy, var_in_copy)
@@ -160,80 +167,124 @@ class decompose:
                 empt += coeff * copied_sub_model.getVarByName(var_in_copy.VarName)
 
             for v in range(len(self.prob.master_vars)):
-                if 'eta' not in self.prob.master_vars[v].getAttr('VarName'):
-                    master_var_in_copy = copied_mean_model.getVarByName(self.prob.master_vars[v].getAttr('VarName'))
-                    Cx += copied_mean_model.getCoeff(constr_in_copy, master_var_in_copy) * incmbt[v]
+                if "eta" not in self.prob.master_vars[v].getAttr("VarName"):
+                    master_var_in_copy = copied_mean_model.getVarByName(
+                        self.prob.master_vars[v].getAttr("VarName")
+                    )
+                    Cx += (
+                        copied_mean_model.getCoeff(constr_in_copy, master_var_in_copy)
+                        * incmbt[v]
+                    )
 
-            copied_sub_model.addConstr(empt, c.getAttr('Sense'), c.getAttr('RHS') - Cx, c.getAttr('ConstrName'))
+            copied_sub_model.addConstr(
+                empt, c.getAttr("Sense"), c.getAttr("RHS") - Cx, c.getAttr("ConstrName")
+            )
             copied_sub_model.update()
 
         self.prob.sub_model = copied_sub_model.copy()
         self.prob.sub_model.update()
-    
-    #Creating linear master with one surrogates (\eta)
+
+    # Creating linear master with one surrogates (\eta)
     def create_master(self):
-        self.prob.master_vars = self.prob.master_vars[:self.tim.stage_idx_col[1]]
-        self.prob.master_const = self.prob.master_const[:self.tim.stage_idx_row[1]]
-        
-        # Create surrogate variables 
+        self.prob.master_vars = self.prob.master_vars[: self.tim.stage_idx_col[1]]
+        self.prob.master_const = self.prob.master_const[: self.tim.stage_idx_row[1]]
+
+        # Create surrogate variables
         for v in self.prob.master_vars:
-            self.prob.master_model.addVar(lb=v.getAttr("LB"), ub=v.getAttr("UB"), obj=v.getAttr("Obj"), vtype=v.getAttr("VType"), name=v.getAttr("VarName"))
+            self.prob.master_model.addVar(
+                lb=v.getAttr("LB"),
+                ub=v.getAttr("UB"),
+                obj=v.getAttr("Obj"),
+                vtype=v.getAttr("VType"),
+                name=v.getAttr("VarName"),
+            )
         self.prob.master_model.update()
         self.prob.master_vars = self.prob.master_model.getVars()
-        eta = self.prob.master_model.addVar ( lb=0.0, ub=gb.GRB.INFINITY, obj=1.0, vtype=gb.GRB.CONTINUOUS, name="\eta") 
+        eta = self.prob.master_model.addVar(
+            lb=0.0, ub=gb.GRB.INFINITY, obj=1.0, vtype=gb.GRB.CONTINUOUS, name="\eta"
+        )
         self.prob.master_model.update()
         self.prob.master_vars.append(eta)
-        
-        #Building the master objective function
+
+        # Building the master objective function
         obj_ = self.prob.mean_model.getObjective()
-        varName = [j.getAttr("VarName") for j in self.prob.master_vars ]
+        varName = [j.getAttr("VarName") for j in self.prob.master_vars]
         newobj_ = gb.LinExpr()
         newobj_ = eta
         for t in range(obj_.size()):
             if obj_.getVar(t).getAttr("VarName") in varName:
-                newobj_ += obj_.getCoeff(t) * self.prob.master_vars[varName.index(obj_.getVar(t).getAttr("VarName"))]
+                newobj_ += (
+                    obj_.getCoeff(t)
+                    * self.prob.master_vars[
+                        varName.index(obj_.getVar(t).getAttr("VarName"))
+                    ]
+                )
         self.prob.master_model.setObjective(newobj_)
         self.create_master_constr()
-    
-    #Creating linear master with multiple surrogates(\eta0,\eta1,...) 
+
+    # Creating linear master with multiple surrogates(\eta0,\eta1,...)
     def create_master_multi(self, scen_num):
-        self.prob.master_vars = self.prob.master_vars[:self.tim.stage_idx_col[1]]
-        self.prob.master_const = self.prob.master_const[:self.tim.stage_idx_row[1]]
-        
-        # Create surrogate variables 
+        self.prob.master_vars = self.prob.master_vars[: self.tim.stage_idx_col[1]]
+        self.prob.master_const = self.prob.master_const[: self.tim.stage_idx_row[1]]
+
+        # Create surrogate variables
         for v in self.prob.master_vars:
-            self.prob.master_model.addVar(lb=v.getAttr("LB"), ub=v.getAttr("UB"), obj=v.getAttr("Obj"), vtype=v.getAttr("VType"), name=v.getAttr("VarName"))
+            self.prob.master_model.addVar(
+                lb=v.getAttr("LB"),
+                ub=v.getAttr("UB"),
+                obj=v.getAttr("Obj"),
+                vtype=v.getAttr("VType"),
+                name=v.getAttr("VarName"),
+            )
         self.prob.master_model.update()
         self.prob.master_vars = self.prob.master_model.getVars()
-        varName = [j.getAttr("VarName") for j in self.prob.master_vars ]
-        eta = self.prob.master_model.addVars(range(scen_num),lb=0.0, ub=gb.GRB.INFINITY, obj=1.0, vtype=gb.GRB.CONTINUOUS, name="\eta") 
+        varName = [j.getAttr("VarName") for j in self.prob.master_vars]
+        eta = self.prob.master_model.addVars(
+            range(scen_num),
+            lb=0.0,
+            ub=gb.GRB.INFINITY,
+            obj=1.0,
+            vtype=gb.GRB.CONTINUOUS,
+            name="\eta",
+        )
         self.prob.master_model.update()
         for v in eta:
             self.prob.master_vars.append(eta[v])
-        
-        #Building the master objective function
+
+        # Building the master objective function
         obj_ = self.prob.mean_model.getObjective()
         newobj_ = gb.LinExpr()
         for v in eta:
             newobj_ += eta[v]
         for t in range(obj_.size()):
             if obj_.getVar(t).getAttr("VarName") in varName:
-                newobj_ += obj_.getCoeff(t) * self.prob.master_vars[varName.index(obj_.getVar(t).getAttr("VarName"))]
+                newobj_ += (
+                    obj_.getCoeff(t)
+                    * self.prob.master_vars[
+                        varName.index(obj_.getVar(t).getAttr("VarName"))
+                    ]
+                )
         self.prob.master_model.setObjective(newobj_)
         self.create_master_constr()
 
-    #creating the Lshaped subproblem
-    def create_LSsub(self,obs,incmb):
+    # creating the Lshaped subproblem
+    def create_LSsub(self, obs, incmb):
         self.prob.sub_model.remove(self.prob.sub_model.getVars())
         self.prob.sub_model.remove(self.prob.sub_model.getConstrs())
 
-        #Create new variables
-        self.prob.sub_vars  = self.prob.mean_vars[self.tim.stage_idx_col[1]:]
+        # Create new variables
+        self.prob.sub_vars = self.prob.mean_vars[self.tim.stage_idx_col[1] :]
 
         for v in self.prob.sub_vars:
-            self.prob.sub_model.addVar(lb=v.getAttr("LB"), ub=v.getAttr("UB"), obj=v.getAttr("Obj"), vtype=v.getAttr("VType"), name=v.getAttr("VarName"))
-        
+            self.prob.sub_model.addVar(
+                lb=v.getAttr("LB"),
+                ub=v.getAttr("UB"),
+                obj=v.getAttr("Obj"),
+                vtype=v.getAttr("VType"),
+                name=v.getAttr("VarName"),
+            )
+
         self.prob.sub_model.update()
         self.prob.sub_vars = self.prob.sub_model.getVars()
 
-        self.create_LSsub_constr(obs,incmb)
+        self.create_LSsub_constr(obs, incmb)
